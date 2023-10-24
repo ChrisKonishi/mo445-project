9afb7512884a660489ee42448bdec9e1b36ba4db
ChrisKonishi <christian.konishi@gmail.com>
2023-10-24 18:48:51 -0300

    feat/relu (#3)
    * add relu
    
    * salie merged folder
    
    * cpp properties

--- encode_layer.c
+++ encode_layer.c
@@ -1,19 +1,18 @@
 #include "ift.h"
 
-/* Author: Alexandre Xavier Falcão (September, 10th 2023) 
+/* Author: Alexandre Xavier Falcão (September, 10th 2023)
 
    Description: Executes a convolutional block to encode the current
-   layer using the model of each training image. 
+   layer using the model of each training image.
 
 */
 
-float *LoadBias(char *basepath)
-{
+float *LoadBias(char *basepath) {
   int number_of_kernels;
   char filename[200];
   FILE *fp;
   float *bias;
-  
+
   sprintf(filename, "%s-bias.txt", basepath);
   fp = fopen(filename, "r");
   fscanf(fp, "%d", &number_of_kernels);
@@ -23,36 +22,30 @@ float *LoadBias(char *basepath)
   }
   fclose(fp);
 
-  return(bias);
+  return (bias);
 }
 
-iftAdjRel *GetPatchAdjacency(iftMImage *mimg, iftFLIMLayer layer)
-{
+iftAdjRel *GetPatchAdjacency(iftMImage *mimg, iftFLIMLayer layer) {
   iftAdjRel *A;
 
-  if (iftIs3DMImage(mimg)){
-    A = iftCuboidWithDilationForConv(layer.kernel_size[0],
-				     layer.kernel_size[1],
-				     layer.kernel_size[2],
-				     layer.dilation_rate[0],
-				     layer.dilation_rate[1],
-				     layer.dilation_rate[2]);
-  }else{
-    A = iftRectangularWithDilationForConv(layer.kernel_size[0],
-					  layer.kernel_size[1],
-					  layer.dilation_rate[0],
-					  layer.dilation_rate[1]);    
+  if (iftIs3DMImage(mimg)) {
+    A = iftCuboidWithDilationForConv(
+        layer.kernel_size[0], layer.kernel_size[1], layer.kernel_size[2],
+        layer.dilation_rate[0], layer.dilation_rate[1], layer.dilation_rate[2]);
+  } else {
+    A = iftRectangularWithDilationForConv(
+        layer.kernel_size[0], layer.kernel_size[1], layer.dilation_rate[0],
+        layer.dilation_rate[1]);
   }
 
-  return(A);
+  return (A);
 }
 
-int *LoadTruelabels(char *filename)
-{
-  int    number_of_kernels;
-  FILE  *fp;
-  int   *truelabel;
-  
+int *LoadTruelabels(char *filename) {
+  int number_of_kernels;
+  FILE *fp;
+  int *truelabel;
+
   fp = fopen(filename, "r");
   fscanf(fp, "%d", &number_of_kernels);
   truelabel = iftAllocIntArray(number_of_kernels);
@@ -61,108 +54,115 @@ int *LoadTruelabels(char *filename)
   }
   fclose(fp);
 
-  return(truelabel);
+  return (truelabel);
 }
 
 int main(int argc, char *argv[]) {
-    timer *tstart;
+  timer *tstart;
 
-    /* Example: encode_layer arch.json 1 flim_models */
-    
-    if (argc!=4)
-      iftError("Usage: encode_layer <P1> <P2> <P3>\n"
-	       "[1] architecture of the network (.json) \n"
-	       "[2] layer number (1, 2, 3) \n"
-	       "[3] folder with the models \n",
-	       "main");
+  /* Example: encode_layer arch.json 1 flim_models */
 
-    tstart = iftTic();
+  if (argc != 4)
+    iftError("Usage: encode_layer <P1> <P2> <P3>\n"
+             "[1] architecture of the network (.json) \n"
+             "[2] layer number (1, 2, 3) \n"
+             "[3] folder with the models \n",
+             "main");
 
-    iftFLIMArch *arch   = iftReadFLIMArch(argv[1]);
-    int          layer  = atoi(argv[2]);
-    char    *model_dir  = argv[3];
-    char    *filename   = iftAllocCharArray(512);
-    char     input_dir[20], output_dir[20]; 
-    
-    sprintf(input_dir,"layer%d",layer-1);
-    sprintf(output_dir,"layer%d",layer);
-    iftMakeDir(output_dir);
+  tstart = iftTic();
 
-    iftFileSet *fs = iftLoadFileSetFromDirBySuffix(input_dir, ".mimg", true);
+  iftFLIMArch *arch = iftReadFLIMArch(argv[1]);
+  int layer = atoi(argv[2]);
+  char *model_dir = argv[3];
+  char *filename = iftAllocCharArray(512);
+  char input_dir[20], output_dir[20];
 
-    for (int i=0; i < fs->n; i++) {
-      iftMImage *mimg  = iftReadMImage(fs->files[i]->path);
-      char *basename   = iftFilename(fs->files[i]->path, ".mimg");
-      sprintf(filename,"%s/%s-conv%d-kernels.npy",model_dir,basename,layer);
+  sprintf(input_dir, "layer%d", layer - 1);
+  sprintf(output_dir, "layer%d", layer);
+  iftMakeDir(output_dir);
 
-      if (iftFileExists(filename)){ /* encode layer using its kernels */
-        iftMatrix *K     = iftReadMatrix(filename);	       
-        sprintf(filename,"%s/%s-conv%d",model_dir,basename,layer);
-        float     *bias  = LoadBias(filename);
-        iftAdjRel *A     = GetPatchAdjacency(mimg, arch->layer[layer-1]);
-        
-        /* Complete the code below to compute convolution with a kernel
-          bank followed by bias */
-        int nkernels = K->ncols;
-        /* Convolution */
-        iftMatrix *XI = iftMImageToFeatureMatrix(mimg,A,NULL); /*row: pixel, col: adjacencia*/
-        iftMatrix *XJ = iftMultMatrices(XI, K); /*row:pixel, col:band*/
-        /* Add bias */
-        for (int i=0; i<nkernels; i++){
-          float c_bias = bias[i];
-          for (int b=0; b<XJ->nrows; b++){
-            iftMatrixElem(XJ, i, b) = iftMatrixElem(XJ, i, b) + c_bias;
-          }
+  iftFileSet *fs = iftLoadFileSetFromDirBySuffix(input_dir, ".mimg", true);
+
+  for (int i = 0; i < fs->n; i++) {
+    iftMImage *mimg = iftReadMImage(fs->files[i]->path);
+    char *basename = iftFilename(fs->files[i]->path, ".mimg");
+    sprintf(filename, "%s/%s-conv%d-kernels.npy", model_dir, basename, layer);
+
+    if (iftFileExists(filename)) { /* encode layer using its kernels */
+      iftMatrix *K = iftReadMatrix(filename);
+      sprintf(filename, "%s/%s-conv%d", model_dir, basename, layer);
+      float *bias = LoadBias(filename);
+      iftAdjRel *A = GetPatchAdjacency(mimg, arch->layer[layer - 1]);
+
+      /* Complete the code below to compute convolution with a kernel
+        bank followed by bias */
+      int nkernels = K->ncols;
+      /* Convolution */
+      iftMatrix *XI = iftMImageToFeatureMatrix(
+          mimg, A, NULL);                     /*row: pixel, col: adjacencia*/
+      iftMatrix *XJ = iftMultMatrices(XI, K); /*row:pixel, col:band*/
+      /* Add bias */
+      for (int i = 0; i < nkernels; i++) {
+        float c_bias = bias[i];
+        for (int b = 0; b < XJ->nrows; b++) {
+          iftMatrixElem(XJ, i, b) = iftMatrixElem(XJ, i, b) + c_bias;
         }
-        iftDestroyMatrix(&XI);
-        iftMImage *activ = iftMatrixToMImage(XJ, mimg->xsize, mimg->ysize, mimg->zsize, nkernels, 'c');
-        iftDestroyMatrix(&XJ);
-        iftDestroyAdjRel(&A);
-        /* Pooling */
-        
-        if (strcmp(arch->layer[layer-1].pool_type, "no_pool") != 0){
-          iftMImage *pool = NULL;
-          if (strcmp(arch->layer[layer-1].pool_type, "avg_pool") == 0) {
-            pool = iftFLIMAtrousAveragePooling(activ,
-                      arch->layer[layer-1].pool_size[0],
-                      arch->layer[layer-1].pool_size[1],
-                      arch->layer[layer-1].pool_size[2],
-                      1,
-                      arch->layer[layer-1].pool_stride);
+      }
+      iftDestroyMatrix(&XI);
+      iftMImage *activ = iftMatrixToMImage(XJ, mimg->xsize, mimg->ysize,
+                                           mimg->zsize, nkernels, 'c');
+      iftDestroyMatrix(&XJ);
+      iftDestroyAdjRel(&A);
+
+      /* Relu */
+      for (int p = 0; p < activ->n; p++) {
+        for (int b = 0; b < activ->m; b++) {
+          activ->val[p][b] = activ->val[p][b] < 0 ? 0 : activ->val[p][b];
+        }
+      }
+
+      /* Pooling */
+
+      if (strcmp(arch->layer[layer - 1].pool_type, "no_pool") != 0) {
+        iftMImage *pool = NULL;
+        if (strcmp(arch->layer[layer - 1].pool_type, "avg_pool") == 0) {
+          pool = iftFLIMAtrousAveragePooling(
+              activ, arch->layer[layer - 1].pool_size[0],
+              arch->layer[layer - 1].pool_size[1],
+              arch->layer[layer - 1].pool_size[2], 1,
+              arch->layer[layer - 1].pool_stride);
+          iftDestroyMImage(&activ);
+          activ = pool;
+        } else {
+          if (strcmp(arch->layer[layer - 1].pool_type, "max_pool") == 0) {
+            pool = iftFLIMAtrousMaxPooling(
+                activ, arch->layer[layer - 1].pool_size[0],
+                arch->layer[layer - 1].pool_size[1],
+                arch->layer[layer - 1].pool_size[2], 1,
+                arch->layer[layer - 1].pool_stride);
             iftDestroyMImage(&activ);
             activ = pool;
           } else {
-            if (strcmp(arch->layer[layer-1].pool_type, "max_pool") == 0) { 
-              pool = iftFLIMAtrousMaxPooling(activ,
-                    arch->layer[layer-1].pool_size[0],
-                    arch->layer[layer-1].pool_size[1],
-                    arch->layer[layer-1].pool_size[2],
-                    1,
-                    arch->layer[layer-1].pool_stride);
-              iftDestroyMImage(&activ);
-              activ = pool;
-            } else {
-              iftError("Invalid pooling in layer %d","main",layer);
-            }
+            iftError("Invalid pooling in layer %d", "main", layer);
           }
         }
-        
-        sprintf(filename,"%s/%s.mimg",output_dir,basename);
-        iftWriteMImage(activ,filename);
-        iftDestroyMatrix(&K);
-        iftDestroyMImage(&activ);
-        iftFree(bias);
       }
-      
-      iftFree(basename);
-      iftDestroyMImage(&mimg);
+
+      sprintf(filename, "%s/%s.mimg", output_dir, basename);
+      iftWriteMImage(activ, filename);
+      iftDestroyMatrix(&K);
+      iftDestroyMImage(&activ);
+      iftFree(bias);
     }
-    
-    
-    iftFree(filename);
-    iftDestroyFileSet(&fs);
-    iftDestroyFLIMArch(&arch);
-    
-    printf("Done ... %s\n", iftFormattedTime(iftCompTime(tstart, iftToc())));
-    return (0);
+
+    iftFree(basename);
+    iftDestroyMImage(&mimg);
+  }
+
+  iftFree(filename);
+  iftDestroyFileSet(&fs);
+  iftDestroyFLIMArch(&arch);
+
+  printf("Done ... %s\n", iftFormattedTime(iftCompTime(tstart, iftToc())));
+  return (0);
 }
