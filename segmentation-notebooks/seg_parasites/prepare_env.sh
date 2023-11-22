1e0c78e2a998cc394bdb99745d35da730aed69c9
Chris Konishi <christian.konishi@gmail.com>
2023-11-22 20:57:12 -0300

    comment

--- segmentation-notebooks/seg_parasites/prepare_env.sh
+++ segmentation-notebooks/seg_parasites/prepare_env.sh
@@ -6,7 +6,7 @@ set -x
 cmd="mamba" # or conda
 
 # create mamba/conda environment
-$cmd create -n mo445 python==3.10
+# $cmd create -n mo445 python==3.10
 
 $cmd install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia -y
 $cmd install matplotlib tqdm numpy tifffile monai torchmetrics jupyter medpy -y
