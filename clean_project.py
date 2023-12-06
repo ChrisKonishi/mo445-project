6edfed2fbd5a677559e948eaf5bb207a01d9e42f
Gabriel Gutierrez <gabriel.bgs00@gmail.com>
2023-12-06 16:11:33 -0300

    Add code to clean project directory

---
+++ clean_project.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+# delete the folders bag, bb, boxes, filtered, flim, iou, label, layer0, layer1, objs, salie
+
+folders_to_delete = ['bag', 'bb', 'boxes', 'filtered', 'flim', 'iou', 'label', 'layer0', 'layer1', 'objs', 'salie']
+
+
+for folder in folders_to_delete:
+    folder_path = './' + folder
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+        print(f"Deleted folder: {folder}")
+    else:
+        print(f"Folder {folder} does not exist")
